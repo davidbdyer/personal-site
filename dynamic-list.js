2d5d25ec6462b05e8f04fcf8d318106eb15afd67
davidbdeath <social_davidbdyer@icloud.com>
2021-10-13 02:07:24 +0000

    removed ul and replaced with div to fix format

---
+++ dynamic-list.js
@@ -0,0 +1,12 @@
+const list = document.querySelector('#dynamic-list');
+
+const listItems = [
+	{
+		url: 'https://prayuplife.com',
+		img: 'images/prayup.jpg',
+		img_title: 'pray up site thumbnail',
+		title: 'Pray Up',
+		discription: 'Designed and implemented website for Pray Up ministry.',
+		year: '2021'
+	}
+]
