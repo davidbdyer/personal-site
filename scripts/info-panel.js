d8c09128ba8df26b834c94f74a57fb7458bc4ba4
davidbdeath <social_davidbdyer@icloud.com>
2021-12-14 20:44:00 +0000

    added project details

--- scripts/info-panel.js
+++ scripts/info-panel.js
@@ -1,9 +1,32 @@
 const main = document.querySelector('main');
-const cards = main.querySelectorAll('.card');
+const cardArray = main.querySelectorAll('.card');
 
 
-cards.forEach((card) => {
-	card.addEventListener('click', (e) => {
-		e.target.style.display = 'none';
-	})
-})
+// utility functions
+const hideContent = (parent, query) => {
+	parent
+		.querySelectorAll(query)
+		.forEach((item) => item.setAttribute("hidden", true));
+}
+
+const showContent = (parent, query) => {
+	parent.querySelector(query).removeAttribute('hidden');
+}
+
+// selects tab and changes visibility
+const changeTabPane = (e) => {
+	const targetTab = e.target;
+	const targetPanel = targetTab.getAttribute('aria-controls')
+
+	hideContent(main, '.project-details');
+	showContent(main, `#${targetPanel}`);
+	showContent(main, '#container--project-details');
+}
+
+
+// event listeners
+const cardTitle = main.querySelectorAll('.card-title');
+
+cardTitle.forEach((elm) => {
+	elm.addEventListener('click', changeTabPane);
+});
