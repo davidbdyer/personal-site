ce2d76ceeb43055958729ec9dc65514e29d8afd0
davidbdeath <social_davidbdyer@icloud.com>
2021-12-17 21:04:18 +0000

    php test

--- scripts/mail.php
+++ scripts/mail.php
@@ -1,12 +1,12 @@
 <?php
 $name = $_POST['name'];
 $email = $_POST['email'];
-$mobile = $_POST['mobile'];
-$message = $_POST['message']
-$formcontent="From: $name \n Email: $email \n Mobile: $mobile \n Message: $message";
+$message = $_POST['message'];
+$subject = $_POST['subject'];
+$formcontent="From: $name \n Email: $email \n Message: $message";
 $recipient = "daviddyer@me.com";
-$subject = "Web Dev Contact Form";
 $mailheader = "From: My Website \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 // echo "Thank You!";
-header('Location: index.html');
+header('Location: https://prayuplife.com/confirmation_page.html');
+
