7c1500c2d683a628b3ccde4060eda72b5ec93144
davidbdyer <social_davidbdyer@icloud.com>
2024-01-21 19:37:36 -0500

    adjusted regedit for name

--- partials/contact-form.php
+++ partials/contact-form.php
@@ -14,7 +14,7 @@ try {
 <div class="contact-form--container container">
 	<h2>Hire Me!</h2>
 	<form method="POST" action="scripts/mail.php" class="contact-form" name="contact-form" id="contact-form">
-		<input aria-label="name" type="text" name="name" id="name" placeholder="name" pattern="^[\w\-\.'\p{L}]{2,30}$" required />
+		<input aria-label="name" type="text" name="name" id="name" placeholder="name" pattern="^[\w\-\.'\p{L} ]{2,30}$" required />
 		<input aria-label="email" type="email" name="email" id="email" placeholder="email" pattern="^[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}$" required />
 		<input aria-label="subject" type="text" name="subject" id="subject" minlength="4" maxlength="50" placeholder="subject" required />
 		<textarea aria-label="message" name="message" id="message" minlength="200" rows="5" placeholder="message" required></textarea>
